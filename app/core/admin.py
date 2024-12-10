177627d9cf2c704d492dfed676e7aca0e9e55340
gsk-007 <gsingh16742@gmail.com>
2024-12-10 21:47:35 +0530

    (feat): add recipe Tag Model

--- app/core/admin.py
+++ app/core/admin.py
@@ -44,3 +44,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin) # You need to specify UserAdmin Class
 admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
